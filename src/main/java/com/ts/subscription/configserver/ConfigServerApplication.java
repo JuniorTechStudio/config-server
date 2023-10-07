5ea242461b5473e690ee6d25d173d05d8731509f
artembichik <a.b.220@yandex.ru>
2023-10-07 18:50:34 +0200

    transition to a Java 21

--- src/main/java/com/ts/subscription/configserver/ConfigServerApplication.java
+++ src/main/java/com/ts/subscription/configserver/ConfigServerApplication.java
@@ -1,10 +1,8 @@
-package com.jts.subscription.configserver;
+package com.ts.subscription.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
--- src/test/java/com/ts/subscription/configserver/ConfigServerApplicationTests.java
+++ src/test/java/com/ts/subscription/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jts.subscription.configserver;
+package com.ts.subscription.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
